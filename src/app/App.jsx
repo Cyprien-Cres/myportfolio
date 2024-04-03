4335989de1da6b885bb036d02cca57ecccc38a5a
Cyprien <cres.cyprien@gmail.com>
2024-04-03 16:42:21 +0200

    Correction bug deploiement

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,48 +1,8 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Header from "../containers/header/Header"
-import Footer from "../containers/footer/Footer"
-import Home from "../pages/home/Home"
-import About from "../pages/about/About"
-import Projects from "../pages/projects/Projects"
-import Error from "../pages/error/Error"
-import "./App.scss"
-
-const router = createBrowserRouter([
-  {
-    path: "/myportfolio/",
-    element: <><Header/><Home/><Footer/></>,
-  },
-  {
-    path: "/myportfolio/about",
-    element: <><Header/><About/><Footer/></>,
-  },
-  {
-    path: "/myportfolio/projects",
-    element: <><Header/><Projects/><Footer/></>,
-  },
-  {
-    path: "/myportfolio/*",
-    element: <><Header/><Error/><Footer/></>,
-  },
-]);
-
-const cursor = document.querySelector('.cursor')
-
-document.addEventListener('mousemove', e => {
-  cursor.setAttribute('style', 'top:'+(e.pageY - 7)+"px; left:"+(e.pageX - 7)+"px;")
-})
-
-document.addEventListener('click', () => {
-  cursor.classList.add('expand')
-
-  setTimeout(() => {
-    cursor.classList.remove("expand")
-  }, 500)
-})
+import { Outlet } from "react-router-dom"
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <Outlet />
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,50 @@
 import ReactDOM from 'react-dom/client'
-import App from './app/App'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Header from "./containers/header/Header"
+import Footer from "./containers/footer/Footer"
+import Home from "./pages/home/Home"
+import About from "./pages/about/About"
+import Projects from "./pages/projects/Projects"
+import Error from "./pages/error/Error"
 import { store } from './app/store'
 import { Provider } from 'react-redux'
+import "./main.scss"
+
+const cursor = document.querySelector('.cursor')
+
+document.addEventListener('mousemove', e => {
+  cursor.setAttribute('style', 'top:'+(e.pageY - 7)+"px; left:"+(e.pageX - 7)+"px;")
+})
+
+document.addEventListener('click', () => {
+  cursor.classList.add('expand')
+
+  setTimeout(() => {
+    cursor.classList.remove("expand")
+  }, 500)
+})
+
+const router = createBrowserRouter([
+  {
+    path: "/myportfolio/",
+    element: <><Header/><Home/><Footer/></>,
+  },
+  {
+    path: "/myportfolio/about",
+    element: <><Header/><About/><Footer/></>,
+  },
+  {
+    path: "/myportfolio/projects",
+    element: <><Header/><Projects/><Footer/></>,
+  },
+  {
+    path: "/myportfolio/*",
+    element: <><Header/><Error/><Footer/></>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <RouterProvider router={router} />
   </Provider>
 )
