07eda0687e7aff0b5635ce8e606efec6582452a0
Cyprien <cres.cyprien@gmail.com>
2024-04-03 16:34:54 +0200

    Correction bug deploiement

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -11,20 +11,18 @@ const router = createBrowserRouter([
   {
     path: "/myportfolio/",
     element: <><Header/><Home/><Footer/></>,
-    children: [
-      {
-        path: "/myportfolio/about",
-        element: <><Header/><About/><Footer/></>,
-      },
-      {
-        path: "/myportfolio/projects",
-        element: <><Header/><Projects/><Footer/></>,
-      },
-      {
-        path: "/myportfolio/*",
-        element: <><Header/><Error/><Footer/></>,
-      },
-    ]
+  },
+  {
+    path: "/myportfolio/about",
+    element: <><Header/><About/><Footer/></>,
+  },
+  {
+    path: "/myportfolio/projects",
+    element: <><Header/><Projects/><Footer/></>,
+  },
+  {
+    path: "/myportfolio/*",
+    element: <><Header/><Error/><Footer/></>,
   },
 ]);
 
